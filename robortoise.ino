f062d3f71824743c8850596ca0b0f71acf552d74
kcmw3e <kcmw3e@gmail.com>
2022-04-27 15:08:26 -0400

    simple stand/sit routine

--- robortoise.ino
+++ robortoise.ino
@@ -6,8 +6,13 @@ Quad robortoise;
 
 void setup() {
     robortoise.init(pins, lims);
+
+    Serial.begin(115200);
 }
 
 void loop() {
-    
+    robortoise.stand();
+    delay(5000);
+    robortoise.belly();
+    delay(5000);
 }

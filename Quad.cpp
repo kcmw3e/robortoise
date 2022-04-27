3ee0c813c38572ac9ececf1506c1c29f9eb8f763
kcmw3e <kcmw3e@gmail.com>
2022-04-27 15:08:01 -0400

    add control functions

--- Quad.cpp
+++ Quad.cpp
@@ -9,4 +9,46 @@ void Quad::init(struct Leg_pins pins[], struct Leg_seg_lims lims[][NUM_SEGS]) {
     leg_FR.init(pins[FR], lims[FR]);
     leg_BL.init(pins[BL], lims[BL]);
     leg_BR.init(pins[BR], lims[BR]);
+
+    legs[FL] = leg_FL;
+    legs[FR] = leg_FR;
+    legs[BL] = leg_BL;
+    legs[BR] = leg_BR;
+
+    leg_FL.hip_bkd();
+    leg_FR.hip_bkd();
+    leg_BL.hip_fwd();
+    leg_BR.hip_fwd();
+}
+
+void Quad::set(int leg, int seg, float set) {
+    Leg L = legs[leg];
+    L.set(seg, set);
+}
+
+void Quad::stand() {
+    set(FL, HIP, 0.5f);
+    set(FL, KNEE, 0.0f);
+    set(FL, FOOT, 0.0f);
+    delay(500);
+
+    set(BL, HIP, 1.0f);
+    set(BL, KNEE, 0.1f);
+    set(BL, FOOT, 0.0f);
+    delay(500);
+
+    set(BR, HIP, 0.5f);
+    set(BL, HIP, 0.0f);
+    set(BR, FOOT, 0.0f);
+    set(BL, FOOT, 0.0f);
+    delay(250);
+    set(BR, KNEE, 0.0f);
+    set(BL, KNEE, 0.0f);
+}
+
+void Quad::belly() {
+    for (int i = 0; i < NUM_LEGS; i++) {
+        Leg leg = legs[i];
+        leg.knee_lift();
+    }
 }
--- Quad.h
+++ Quad.h
@@ -6,6 +6,8 @@
 
 #include "Leg.h"
 
+#define NUM_LEGS 4
+
 enum LEG_INDEX {
     FL = 0,
     FR = 1,
@@ -19,10 +21,14 @@ class Quad {
         Leg leg_FR;
         Leg leg_BL;
         Leg leg_BR;
+        Leg legs[NUM_LEGS];
 
     public:
     Quad();
     void init(struct Leg_pins pins[], struct Leg_seg_lims lims[][NUM_SEGS]);
+    void set(int leg, int seg, float set);
+    void stand();
+    void belly();
 };
 
 #endif // ROBORTOISE_QUAD_H
